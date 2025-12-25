d98641c9d4a235ed3bf6239ebbfe57b4479824b2
Keloran <keloran@develbox.info>
2025-12-25 19:40:32 +0000

    see if this works

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.serialization") version "1.9.23"
     `maven-publish`
+    signing
     id("org.jetbrains.dokka") version "1.9.20"
 }
 
@@ -53,25 +54,27 @@ tasks.withType<KotlinCompile> {
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
+    withSourcesJar()
+    withJavadocJar()
 }
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
-            
+
             pom {
                 name.set("Flags.gg Kotlin Client")
                 description.set("Kotlin client library for the flags.gg feature flag management system")
                 url.set("https://github.com/flags-gg/kotlin-flags")
-                
+
                 licenses {
                     license {
                         name.set("MIT License")
                         url.set("https://opensource.org/licenses/MIT")
                     }
                 }
-                
+
                 developers {
                     developer {
                         id.set("flags-gg")
@@ -79,7 +82,32 @@ publishing {
                         email.set("support@flags.gg")
                     }
                 }
+
+                scm {
+                    connection.set("scm:git:git://github.com/flags-gg/kotlin-flags.git")
+                    developerConnection.set("scm:git:ssh://github.com/flags-gg/kotlin-flags.git")
+                    url.set("https://github.com/flags-gg/kotlin-flags")
+                }
             }
         }
     }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    val signingKeyId = System.getenv("SIGNING_KEY_ID")
+    val signingKey = System.getenv("SIGNING_KEY")
+    val signingPassword = System.getenv("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    sign(publishing.publications["maven"])
 }
